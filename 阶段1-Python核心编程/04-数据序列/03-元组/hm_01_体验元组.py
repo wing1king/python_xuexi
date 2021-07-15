ef06e5d03adfffb073d94d0b1fc99cd61c73d7b7
wing1king <zhang4475153@126.com>
2021-07-15 15:50:43 +0800

    04-03元组完成

---
+++ 阶段1-Python核心编程/04-数据序列/03-元组/hm_01_体验元组.py
@@ -0,0 +1,4 @@
+t1 = (10, 20, 30)
+print(t1)
+
+print(type(t1))
---
+++ 阶段1-Python核心编程/04-数据序列/03-元组/hm_03_元组常见操作.py
@@ -0,0 +1,15 @@
+t1 = ('aa', 'bb', 'cc')
+
+# 1.下标
+print(t1[0])
+
+# 2.index()
+# print(t1.index('bb'))   # 1
+# print(t1.index('bbb'))  # 报错
+
+# 3.count()
+# print(t1.count('aa'))   # 1
+# print(t1.count('aaa'))   # 0
+
+# 4.len()
+print(len(t1))  # 3
