d7a278c60512730b3a906d4b54822eaf21113efe
YunFeng <zhang4475153@126.com>
2021-07-17 09:35:55 +0800

    06已完成

---
+++ 阶段1-Python核心编程/04-数据序列/06-公共操作/hm_07_公共方法之range.py
@@ -0,0 +1,15 @@
+# print(range(1, 10, 2))
+# for i in range(1, 10, 1):
+#     print(i)
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(10):
+    print(i)
+
+# 1. 如果不写开始，默认从0开始
+# 2. 如果不写步长，默认为1
